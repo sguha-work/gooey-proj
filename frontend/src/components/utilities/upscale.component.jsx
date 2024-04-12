429c8411cd0aa7779b57b6b69b1f1ad96a478f54
sguha-work <sguha1988.life@gmail.com>
2024-04-12 19:23:00 +0530

    changes

--- frontend/src/components/utilities/upscale.component.jsx
+++ frontend/src/components/utilities/upscale.component.jsx
@@ -53,43 +53,43 @@ const Upscale = () => {
       })();
     });
     Subject_ImageConvertedToRetro$.asObservable().subscribe((retroImageData) => {
-      retroImageData&&(()=>{
+      retroImageData && (() => {
         console.log({ retroImageData });
         const image2 = retroImageData.data.output.output_images[0];
         image = {
           ...image,
           image2
         };
-        setLeftAndRightImageURL({...image});
+        setLeftAndRightImageURL({ ...image });
         Subject_ShowModal$.next(false);
       })()
     });
   }, []);
   return (
     <>
-    <Modal show={true}></Modal>
-    <section className="pt-10 pb-20">
-      <div className="container mx-auto px-5">
-        <div className="flex flex-wrap items-center -mx-5 gap-y-8">
-          <div className="md:w-5/12 w-full px-5">
-            <h3 className="mb-5 lg:text-[45px] text-3xl leading-tight font-bold max-w-[658px]">
-              Upscale pictures
-              <br />
-              without losing quality up to 16x
-            </h3>
-            <p className="text-lg text-gray-700 max-w-[480px]">
-              Make your pics high resolution - HD, 4k and beyond. Enlarge and
-              sharpen photos for printing and web in a single click.
-            </p>
-          </div>
-          <div className="md:w-6/12 w-full px-5 ms-auto">
-            <div className="rounded-2xl border-4 border-solid border-white shadow-[0px_100px_60px_-70px_rgba(19,15,48,0.25)] overflow-hidden">
-              <ReactCompareImage leftImage={leftAndRightImageURL.image1} rightImage={leftAndRightImageURL.image2} />
+      <Modal show={true}></Modal>
+      <section className="pt-10 pb-20">
+        <div className="container mx-auto px-5">
+          <div className="flex flex-wrap items-center -mx-5 gap-y-8">
+            <div className="md:w-5/12 w-full px-5">
+              <h3 className="mb-5 lg:text-[45px] text-3xl leading-tight font-bold max-w-[658px]">
+                Upscale pictures
+                <br />
+                without losing quality up to 16x
+              </h3>
+              <p className="text-lg text-gray-700 max-w-[480px]">
+                Make your pics high resolution - HD, 4k and beyond. Enlarge and
+                sharpen photos for printing and web in a single click.
+              </p>
+            </div>
+            <div className="md:w-6/12 w-full px-5 ms-auto">
+              <div className="rounded-2xl border-4 border-solid border-white shadow-[0px_100px_60px_-70px_rgba(19,15,48,0.25)] overflow-hidden">
+                <ReactCompareImage leftImage={leftAndRightImageURL.image1} rightImage={leftAndRightImageURL.image2} />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
     </>
   );
 };
