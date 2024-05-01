d83c39246468fd56c61f689c374732e3cccca860
khavinshankar <khavinshankar@gmail.com>
2024-05-01 11:50:47 +0530

    added ssl config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  preview: {
+    https: {
+      key: process.env.SSL_KEY_PATH,
+      cert: process.env.SSL_CERT_PATH,
+    },
+  },
+  server: {
+    https: {
+      key: process.env.SSL_KEY_PATH,
+      cert: process.env.SSL_CERT_PATH,
+    },
+  },
   plugins: [react()],
-})
+});
