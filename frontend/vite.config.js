1a63e53780e0f3ec1c0f06c1ee051bfc84a187dd
khavinshankar <khavinshankar@gmail.com>
2024-05-01 12:11:19 +0530

    fix the ssl read issue in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import fs from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   preview: {
     https: {
-      key: process.env.SSL_KEY_PATH,
-      cert: process.env.SSL_CERT_PATH,
+      key: fs.readFileSync(process.env.SSL_KEY_PATH),
+      cert: fs.readFileSync(process.env.SSL_CERT_PATH),
     },
   },
   server: {
     https: {
-      key: process.env.SSL_KEY_PATH,
-      cert: process.env.SSL_CERT_PATH,
+      key: fs.readFileSync(process.env.SSL_KEY_PATH),
+      cert: fs.readFileSync(process.env.SSL_CERT_PATH),
     },
   },
   plugins: [react()],
